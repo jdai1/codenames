32b0f499cfed54e419c7d7d50d4c86b124ec9374
Brian Travis <bztravis88@gmail.com>
2025-11-08 11:11:19 -0800

    Add scaffolding

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,22 @@
 function App() {
-  return <div className='black'>hello world</div>
+  const nCards = 5
+  const mCards = 5
+
+  return (
+    <div className='flex flex-col h-screen'>
+      <div className='h-24'>players</div>
+
+      <div className='grid grid-cols-5 h-full'>
+        <div className='col-span-1 bg-amber-300'>hello world</div>
+        <div className='col-span-3 bg-amber-400'>
+          <div className={`grid grid-cols-${mCards} grid-rows-${nCards}`}>
+            {Array(25).fill(<div>hello</div>)}
+          </div>
+        </div>
+        <div className='col-span-1 bg-amber-700'>hello world</div>
+      </div>
+    </div>
+  )
 }
 
 export default App
